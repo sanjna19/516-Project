c0f1a8b3fc3753dd48ebe52e114cd09a97767bc4
sanjna19 <schipp3@uic.edu>
2025-04-16 12:05:14 -0500

    reweighting with SMOTE to improve class 0 recall

--- src/modeling.py
+++ src/modeling.py
@@ -3,6 +3,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 import pandas as pd
+from imblearn.over_sampling import SMOTE
 
 def train_logistic_regression(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
@@ -19,6 +20,15 @@ def train_random_forest(X, y, sample_weight=None):
         model.fit(X_train, y_train, sample_weight=sample_weight)
     else:
         model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    return model, X_test, y_test, y_pred
+
+def train_smote_forest(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    smote = SMOTE(random_state=42)
+    X_train_smote, y_train_smote = smote.fit_resample(X_train, y_train)
+    model = RandomForestClassifier(class_weight='balanced', random_state=42)
+    model.fit(X_train_smote, y_train_smote)
     y_pred = model.predict(X_test)
     return model, X_test, y_test, y_pred
 
